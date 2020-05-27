fd3e8b92f4a4716eb32caccacbaf90a00d9bc96e
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-27 20:06:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_word = params[:user_phrase]
+    @words = PigLatinizer.new
+    @pig_latin = @words.piglatinize(user_word)
+
+    erb :piglatinize
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,34 @@
+class PigLatinizer
+  attr_accessor :words
+
+  def initialize
+    @words = words
+  end
+
+  def piglatinize(words)
+    text_array = words.split(" ")
+    pig_array = []
+
+    text_array.collect do |word|
+      if word.start_with?(/[AEIOUaeiou]/)
+        pig_word = word + "way"
+        pig_array << pig_word
+      elsif word.start_with?(/[AEIOUaeiou]/)==false && word[1].start_with?(/[AEIOUaeiou]/)==false && word[2].start_with?(/[AEIOUaeiou]/)==false
+        three_letters = word.slice(0..2)
+        pig_word = word.slice(3..-1) + three_letters + "ay"
+        pig_array << pig_word
+      elsif word.start_with?(/[AEIOUaeiou]/)==false && word[1].start_with?(/[AEIOUaeiou]/)==false
+        two_letters = word.slice(0..1)
+        pig_word = word.slice(2..-1) + two_letters + "ay"
+        pig_array << pig_word
+      else
+        first_letter = word.slice(0)
+        pig_word = word.slice(1..-1) + first_letter + "ay"
+        pig_array << pig_word
+      end
+    end
+
+    pig_array.join(" ")
+  end
+
+end
