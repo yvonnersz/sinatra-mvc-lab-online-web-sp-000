3055839794a0a3869923741bc8bad8be08411903
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-27 19:00:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
 
-  # get '/user_input' do
-  #   erb :user_input
-  # end
-  #
-  # post '/piglatinize' do
-  #   @translated_text = PigLatinizer.new(params[:user_input])
-  #
-  #   erb :piglatinize
-  # end
+  get '/user_input' do
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+    @translated_text = PigLatinizer.new(params[:user_input])
+  
+    erb :piglatinize
+  end
 
 end
